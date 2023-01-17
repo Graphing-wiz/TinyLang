ad84bc657626f875166ac0a2ee173b2f0da623a8
Mason A. Dean <100883553+Graphing-wiz@users.noreply.github.com>
2023-01-17 17:22:00 -0500

    Add files via upload

--- interpreter.py
+++ interpreter.py
@@ -21,6 +21,7 @@ import time
 def interactive(cmdd, point, returnB):
     if cmdd:
         cmd = cmdd
+
         for command in cmd:
             command = command.strip()
             if command.startswith('$DATA'):
@@ -28,8 +29,6 @@ def interactive(cmdd, point, returnB):
                 cmd[cmd.index(command)] = str(Data[int(tcmd[1])])
             elif command.startswith('$EVAL'):
                 tcmd = command.split('$EVAL')
-                print(str(cmd.index(command))+'      <INDEX')
-                print(cmd[cmd.index(command)]+'      <VALUE')
                 cmd[cmd.index(command)] = str(interactive(Data[int(tcmd[1])].split(' '), 0, False)).strip()
         if str(None) in str(cmd):
             return cmd.remove(None)
@@ -131,24 +130,22 @@ def interactive(cmdd, point, returnB):
             return Data[point]
         elif cmd[0] == 'tape':
             return Data
-        elif cmd[0] in Functions:
-            temp = ''
-            for x in range(1, len(cmd)):
-                temp+=str(cmd[x]).strip()
-            code = Functions[cmd[0]].replace('\n', '').split('then')
-            for command in code:
-                print(interactive(command.strip().split(' '), 0, True))
         else:
-            print('''%s
+            if str(cmd).strip() in Functions:
+                code = Functions[cmd.strip()].split(' then ')
+                for command in code:
+                    print(interactive(command.split(' '), 0, True))
+            else:
+                print('''%s
     \033[1m Unknown command\033[0m \033[0m  [%s]
     \033[1m \033[93m Error at line\033[0m \033[0m  [%s]
     \033[1m \033[93m File:\033[0m \033[0m  [%s]
     \033[1m \033[93m Path:\033[0m \033[0m  [%s]
     %s''' % (bcolors.FAIL, cmd, 'LOCALLINE', 'LOCAL FILE', os.getcwd(), bcolors.ENDC))
-        if returnB:
-            pointer = point
+            if returnB:
+                pointer = point
 
-        pointer = point
+    pointer = point
 if not 1 < len(sys.argv):
     print('''\033[91m\033[1mPLEASE SPECIFY A OPTION 
     [ -H HELP ON UNKNOWN COMMANDS  ____
@@ -156,7 +153,7 @@ if not 1 < len(sys.argv):
     [ -F FILE READ MODE            |[]|
                                    [||]
                                   ///\\\\\\
-                                   |[]|
+                                  [/[]\\]
                                   {____}
                                  [||[]||]
                                 //|====|\\\\
@@ -165,82 +162,97 @@ if not 1 < len(sys.argv):
 else:
     if sys.argv[1] == '-H':
         print('''
+rest:
+    Halts the process for a given amount of time in seconds
+    Ex: `rest 10`
+    Ex: `rest $DATA01
+
+rand:
+    Outputs a sudo random number based on one minimum and one maximum
+    Ex: `rand 1 100`
+    Ex: `rand $DATA0 $DATA1`
+
+define:
+    Creates a function that will be executed when the name is sent as a command different lines are seperated by a 'then' statement
+    Ex: `define FunctionName print hello then print world then print !`
+    Then if you call the function: `FunctionName` the code will be executed
+
 length:
     Outputs the length of a given argument
     Ex: `length abcdefg`
 
 tape:
-	Outputs the current DATA tape
-	Ex: `tape`
+    Outputs the current DATA tape
+    Ex: `tape`
 
 out:
-	Outputs the current data block at the pointer index
-	Ex: `out`
+    Outputs the current data block at the pointer index
+    Ex: `out`
 
 DATA or $DATA<data index>:
-	Outputs the data block of the given argument
-	Ex: `DATA 0`
-	Ex: `print $DATA0`
+    Outputs the data block of the given argument
+    Ex: `DATA 0`
+    Ex: `print $DATA0`
 
 
 data:
-	Establishes a new data block of either type int (integer), str (string), or a bool (boolean)
-	Ex: `data int 2`
-	Ex: `data str hello world`
-	Ex: `data bool True`
+    Establishes a new data block of either type int (integer), str (string), or a bool (boolean)
+    Ex: `data int 2`
+    Ex: `data str hello world`
+    Ex: `data bool True`
 
 if:
-	Test if a given data block or argument is True if it is true it will excute a set of commands
-	Ex: `if True do out`
-	Ex: `if $DATA0 do out`
+    Test if a given data block or argument is True if it is true it will excute a set of commands
+    Ex: `if True do out`
+    Ex: `if $DATA0 do out`
 
 is:
-	Test if a given expression is true or false
-	Ex: `is 1 + 1 == 2`
-	Ex: `is 9 > 9`
-	It is also possible to assign the output of these commands to a boolean data block
-	Ex: `data str is 1 + 1 == 2` then `data bool $EVAL0`
+    Test if a given expression is true or false
+    Ex: `is 1 + 1 == 2`
+    Ex: `is 9 > 9`
+    It is also possible to assign the output of these commands to a boolean data block
+    Ex: `data str is 1 + 1 == 2` then `data bool $EVAL0`
 
 print:
-	Outputs text or data blocks from given arguments
-	Ex: `print hello world`
-	Ex: `print $DATA0`
-	Ex: `print $EVAL0`
+    Outputs text or data blocks from given arguments
+    Ex: `print hello world`
+    Ex: `print $DATA0`
+    Ex: `print $EVAL0`
 
 loop:
-	Loops and does something multiple time based on given arguments
-	Ex: `loop 3 print hewo`
-	Ex: `loop $DATA0 print hewo`
-	Ex: `loop $DATA0 print $DATA0`
+    Loops and does something multiple time based on given arguments
+    Ex: `loop 3 print hewo`
+    Ex: `loop $DATA0 print hewo`
+    Ex: `loop $DATA0 print $DATA0`
 
 location:
-	Outputs the current pointer index
-	Ex: `location`
+    Outputs the current pointer index
+    Ex: `location`
 
 set:
-	Sets the current data block at the pointer index to a different value based on given arguments
-	Ex: `set int 420`
+    Sets the current data block at the pointer index to a different value based on given arguments
+    Ex: `set int 420`
     (Note, that when setting the int data type the value it is set to is first evaluated)
-	Ex: `set bool True`
-	Ex: `set str Hello, World`
+    Ex: `set bool True`
+    Ex: `set str Hello, World`
 
 math:
-	Evaluates a math equasion from a given set of arguments
-	Ex: `math 1 + 1`
-	(Spaces are optional seing as the mathmatical operations are ran through python3)
-	All allowed operators are:  + - * / and ()
+    Evaluates a math equasion from a given set of arguments
+    Ex: `math 1 + 1`
+    (Spaces are optional seing as the mathmatical operations are ran through python3)
+    All allowed operators are:  + - * / and ()
 
 decrament:
-	Decraments the pointer by a value of 1
-	Ex: `decrament`
+    Decraments the pointer by a value of 1
+    Ex: `decrament`
 
 incrament:
-	Incraments the pointer by a value of 1
-	Ex: `incament`
+    Incraments the pointer by a value of 1
+    Ex: `incament`
 
 $EVAL:
-	Evals a pice of code from a string given as an argument
-	Ex: `data str print hello world` then `$EVAL0`
+    Evals a pice of code from a string given as an argument
+    Ex: `data str print hello world` then `$EVAL0`
     (Note, code no longer prints the value. The output of this code will be an error saying there is no command named hello!)
         ''')
     if sys.argv[1] == '-I':
